1df9089276e7d179ed2fd43f0dc41beea67abdfe
Yamila Moreno <yamila.ms@gmail.com>
2014-02-01 16:36:47 +0100

    Little fix to make both pins needed customizable

--- AirQuality_Sensor/AirQuality.cpp
+++ AirQuality_Sensor/AirQuality.cpp
@@ -1,9 +1,9 @@
 /*
   AirQuality library v1.0
   2010 Copyright (c) Seeed Technology Inc.  All right reserved.
- 
+
   Original Author: Bruce.Qin
-  
+
   This library is free software; you can redistribute it and/or
   modify it under the terms of the GNU Lesser General Public
   License as published by the Free Software Foundation; either
@@ -27,21 +27,22 @@ void AirQuality::avgVoltage()
 {
 	if(i==150)//sum 5 minutes
 	{
-		vol_standard=temp/150; 
+		vol_standard=temp/150;
 		temp=0;
-		Serial.print("Vol_standard in 5 minutes:");			
-		Serial.println(vol_standard);		
-		i=0;		
+		Serial.print("Vol_standard in 5 minutes:");
+		Serial.println(vol_standard);
+		i=0;
 	}
-    else 
+    else
 	{
 		temp+=first_vol;
 		i++;
 	}
 }
-void AirQuality::init(int pin)
+void AirQuality::init(int pin, int pin2)
 {
     _pin=pin;
+    _pin2=pin2;
     pinMode(_pin,INPUT);
     unsigned char i=0;
     Serial.println("sys_starting...");
@@ -53,7 +54,7 @@ void AirQuality::init(int pin)
     {
         if(init_voltage<798 && init_voltage>10)// the init voltage is ok
         {
-            first_vol=analogRead(A0);//initialize first value
+            first_vol=analogRead(_pin2);//initialize first value
             last_vol=first_vol;
             vol_standard=last_vol;
             Serial.println("Sensor ready.");
@@ -61,11 +62,11 @@ void AirQuality::init(int pin)
             break;
         }
         else if(init_voltage>798||init_voltage<=10)
-        {	
+        {
             i++;
             delay(60000);//60000
             Serial.println("waitting sensor init..");
-            init_voltage=analogRead(A0);
+            init_voltage=analogRead(_pin2);
             if(i==5)
             {
                 i=0;
@@ -73,10 +74,10 @@ void AirQuality::init(int pin)
                 Serial.println("Sensor Error!");
             }
           }
-        else 
+        else
         break;
     }
-    //init the timer 
+    //init the timer
     TCCR2A=0;//normal model
     TCCR2B=0x07;//set clock as 1024*(1/16M)
     TIMSK2=0x01;//enable overflow interrupt
@@ -89,34 +90,34 @@ int AirQuality::slope(void)
 	{
     	if(first_vol-last_vol>400||first_vol>700)
         {
-            Serial.println("High pollution! Force signal active.");		
-            timer_index=0;	
-            avgVoltage();	
-            return 0;	
+            Serial.println("High pollution! Force signal active.");
+            timer_index=0;
+            avgVoltage();
+            return 0;
         }
     	else if((first_vol-last_vol>400&&first_vol<700)||first_vol-vol_standard>150)
-        {	
-            Serial.print("sensor_value:");		
-            Serial.print(first_vol);      		
-            Serial.println("\t High pollution!");		
-            timer_index=0;	
+        {
+            Serial.print("sensor_value:");
+            Serial.print(first_vol);
+            Serial.println("\t High pollution!");
+            timer_index=0;
             avgVoltage();
             return 1;
-            
+
         }
     	else if((first_vol-last_vol>200&&first_vol<700)||first_vol-vol_standard>50)
         {
             //Serial.println(first_vol-last_vol);
             Serial.print("sensor_value:");
-            Serial.print(first_vol);      		
-            Serial.println("\t Low pollution!");		
+            Serial.print(first_vol);
+            Serial.println("\t Low pollution!");
             timer_index=0;
             avgVoltage();
-            return 2;	
+            return 2;
         }
     	else
         {
-            avgVoltage();	
+            avgVoltage();
             Serial.print("sensor_value:");
             Serial.print(first_vol);
             Serial.println("\t Air fresh");
